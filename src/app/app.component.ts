ae94c835ceae936ed2905105311e97c5b74116e8
Zita Szupera <szuperaz@gmail.com>
2023-02-08 11:31:52 +0100

    feat: Update to latest stream-chat-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,35 @@
-import { Component, OnInit } from "@angular/core";
-import { environment } from "src/environments/environment";
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import {
   ChatClientService,
   ChannelService,
   StreamI18nService,
-} from "stream-chat-angular";
+  MessageInputConfigService,
+} from 'stream-chat-angular';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   constructor(
     private chatService: ChatClientService,
     private channelService: ChannelService,
-    private streamI18nService: StreamI18nService
+    private streamI18nService: StreamI18nService,
+    private inputConfigService: MessageInputConfigService
   ) {
     const apiKey = environment.apiKey;
     const userId = environment.userId;
     const userToken = environment.userToken;
     this.chatService.init(apiKey, userId, userToken);
     this.streamI18nService.setTranslation();
+    this.inputConfigService.inputMode = 'mobile';
   }
 
   async ngOnInit() {
     this.channelService.init({
-      type: "messaging",
+      type: 'messaging',
       id: { $in: [environment.channelId] },
     });
   }
